7a27d55181d03bce4f1ee17739bb2ab99c4f4ede
Mac_Developer <araozangel842@gmail.com>
2022-05-25 22:09:32 +0300

    Types

---
+++ api/src/types/product.type.ts
@@ -0,0 +1,20 @@
+import { Document } from 'mongoose'
+
+export type IProduct = Document & {
+  title: string
+  price: number
+  description: string
+  image: string
+  category: string
+  quantity?: number
+}
+
+type MatchByName = {
+  name: string
+}
+type MatchByCategory = {
+  category: string
+}
+export type PiplineMatchStage = {
+  $match: MatchByName | MatchByCategory
+}
