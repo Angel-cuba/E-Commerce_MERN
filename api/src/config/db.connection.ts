6856e7febb92d7a9db640c9b2c6265eb8f8a3ef3
Mac_Developer <araozangel842@gmail.com>
2022-05-25 22:02:28 +0300

    Db connection

--- api/src/config/db.connection.ts
+++ api/src/config/db.connection.ts
@@ -1,14 +1,14 @@
-import mongoose, {ConnectOptions} from 'mongoose'
+import mongoose, { ConnectOptions } from 'mongoose'
 import keys from './keys'
 
 type ConnectionOptions = {
-    useNewUrlParser: boolean,
-    useUnifiedTopology: boolean,
+  useNewUrlParser: boolean
+  useUnifiedTopology: boolean
 }
 
 const dbOptions: ConnectionOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }
 
 mongoose.connect(keys.db_URI, dbOptions as ConnectOptions)
@@ -16,12 +16,10 @@ mongoose.connect(keys.db_URI, dbOptions as ConnectOptions)
 const connection = mongoose.connection
 
 connection.once('open', () => {
-    console.log('MongoDB database connection established successfully')
-}
-)
+  console.log('MongoDB database connection established successfully')
+})
 
 connection.on('error', (err) => {
-    console.log('Connection error: ' + err)
-    process.exit(-1)
-}
-)
+  console.log('Connection error: ' + err)
+  process.exit(-1)
+})
--- api/src/config/keys.ts
+++ api/src/config/keys.ts
@@ -1,7 +1,6 @@
-import 'dotenv/config';
+import 'dotenv/config'
 
-
-export default{
+export default {
   db_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.lquq3.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
   PORT: process.env.PORT,
   PRIVATE_KEY: process.env.PRIVATE_KEY,
