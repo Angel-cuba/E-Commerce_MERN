ddf248412f40567f0e99ee7eab75e49aa4894136
Mac_Developer <araozangel842@gmail.com>
2022-05-25 22:08:45 +0300

    Services

---
+++ api/src/services/user.ts
@@ -0,0 +1,29 @@
+import User, { IUser } from '../models/User'
+
+const createUser = async (user: IUser): Promise<IUser> => {
+  return user.save()
+}
+
+const getUserById = async (id: string): Promise<IUser | null> => {
+  return User.findById(id)
+}
+
+const updateAnUser = async (id: string, user: IUser): Promise<IUser | null> => {
+  return User.findByIdAndUpdate(id, user)
+}
+
+const deleteAnUser = async (id: string): Promise<IUser | null> => {
+  return User.findByIdAndDelete(id)
+}
+
+const getAllUsers = async (): Promise<IUser[]> => {
+  return User.find({ role: 'USER' })
+}
+
+export default {
+  createUser,
+  getUserById,
+  updateAnUser,
+  deleteAnUser,
+  getAllUsers,
+}
