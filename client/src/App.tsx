b2c98ecee04f1e7467ebc414fbdb5322521aa6a4
Mac_Developer <araozangel842@gmail.com>
2022-05-26 22:28:07 +0300

    Navbar and Light/Dark with useContext hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import './styles/App.scss';
 import { Routes, Route} from 'react-router-dom';
 import Admin from './pages/Admin';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 
 export default function App() {
   const {theme} = useTheme()
   return (
     <div className={theme === 'light' ? 'Principal': 'Principal-Dark'}>
+      <Navbar/>
      <Routes>
       <Route path="/admin" element={<Admin />} />  
       <Route  path="/" element={<Home />}/>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/components/Navbar.scss'
+import { ToggleTheme } from './ToggleTheme'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div  className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <h1>Logo</h1>
+          </div>
+        <div className="navbar-menu">
+          <ToggleTheme/>
+          <ul>
+            <li>
+              <Link to="#">Home</Link>
+            </li>
+            <li>
+              <Link to="#">About</Link>
+            </li>
+            <li>
+              <Link to="#">Contact</Link>
+            </li>
+          </ul>
+            </div>
+            </div>
+    </div>
   )
 }
 
