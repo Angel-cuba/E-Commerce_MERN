97fe9abc3472c7b751d2bf9a99a45ee5525b4f34
Mac_Developer <araozangel842@gmail.com>
2022-06-03 15:52:23 +0300

    Redirecting user if is authenticated by google

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google'
 import axios from 'axios'
 import '../styles/pages/Login.scss'
-
+import { useNavigate } from 'react-router-dom'
 
 
 const Login = () => {
+  const navigate = useNavigate()
   const handleGoogleResponse = async (response: any) => {
     const idToken = response.credential
+    
 
   const res = await  axios.post('http://localhost:3001/users/signin-google', 
     {},
@@ -16,6 +18,12 @@ const Login = () => {
         Authorization: `Bearer ${idToken}`
       }
     })
+    if(res.data.token){
+      navigate('/home')
+    }
+    else{
+      navigate('/login')
+    }
     localStorage.setItem('token', res.data.token)
   }
 
