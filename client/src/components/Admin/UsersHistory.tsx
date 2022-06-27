06e632f68dd38ac7a53c82b6ab387b00f99bd328
Mac_Developer <araozangel842@gmail.com>
2022-06-27 22:03:39 +0300

    Feyching unique user with purchases

--- client/src/components/Admin/UsersHistory.tsx
+++ client/src/components/Admin/UsersHistory.tsx
@@ -5,32 +5,54 @@ import { AppState } from '../../types/ProductType';
 import Navbar from '../Navbar';
 import '../../styles/components/User/UsersHistory.scss';
 import { Link } from 'react-router-dom';
+import Loading from '../Loading';
 
 const UsersHistory = () => {
   const { user } = useSelector((state: AppState) => state.user);
-  const [fetchHistory, setFetchHistory] = React.useState<any>();
+  const [fetchHistory, setFetchHistory] = React.useState<any>(null);
+  const [history, setHistory] = React.useState<any>(null);
+  console.log(history);
   console.log('the history of all users', fetchHistory);
 
   React.useEffect(() => {
     getAllHistory(user?.email).then((res) => setFetchHistory(res));
+
   }, [user]);
+
+  if (!fetchHistory) {
+    return <Loading />;
+  }
+
+
+  const PurchasesByUser = (thisUserId: any) => {
+    console.log('click', thisUserId);
+    const response = fetchHistory.filter((purchase: any) => {
+      if (purchase.user._id === thisUserId) {
+        return purchase.products;
+      }
+      return null;
+    });
+    setHistory(response);
+  };
+
+  //Fetching uniques users
+  const ids = fetchHistory.map((x: any) => x.user._id);
+  const filtered = fetchHistory.filter(
+    (history: any, index: number) => !ids.includes(history.user._id, index + 1)
+  );
 
   return (
     <div className="users">
       <Navbar />
-      {/* <div className="back"> */}
       <Link to="/admin">Back</Link>
-      {/* </div> */}
       <div className="user_content">
         {!fetchHistory ? (
-          <h1>Loading.....</h1>
+          <Loading />
         ) : (
-          fetchHistory.map((product: any, index: number) =>
-            // fetchHistory && fetchHistory.map((product: any, index: number) =>
-
-            {
-              return (
-                <div key={index} className="user">
+          filtered.map((product: any, index: number) => {
+            return (
+              <div key={index} className="user">
+                <div className="user-face">
                   <div className="user-image">
                     <img src={product.user.picture} alt="user" />
                   </div>
@@ -39,14 +61,13 @@ const UsersHistory = () => {
                       {product.user.name} {product.user.lastname}{' '}
                     </h1>
                   </div>
-                  {/* <div className="email">
-                  <h2>{product.user.email}</h2>
-                  <span>{product.user.role}</span>
-               </div> */}
                 </div>
-              );
-            }
-          )
+                <div className="user-history" onClick={() => PurchasesByUser(product.user._id)}>
+                  <h3>Information</h3>
+                </div>
+              </div>
+            );
+          })
         )}
       </div>
     </div>
